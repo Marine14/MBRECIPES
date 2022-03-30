9cc199d02dab06c4d34c968543d4612ec5f3a907
Marine14 <marinefavier14@gmail.com>
2022-03-30 08:52:21 +0200

    Initial Commit

---
+++ MBRECIPES/MBRECIPESApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBRECIPESApp.swift
+//  MBRECIPES
+//
+//  Created by faviemar on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MBRECIPESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
